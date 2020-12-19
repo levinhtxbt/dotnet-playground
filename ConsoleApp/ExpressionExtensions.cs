f84d6f481213adc9f57c03d1f5bce3288819d7d0
Vinh Le <levinhtxbt@gmail.com>
2020-12-19 08:59:00 +0700

    init

---
+++ ConsoleApp/ExpressionExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace ConsoleApp
+{
+    public static class ExpressionExtensions
+    {
+        public static List<string> GetProperties<T>() where T : class
+        {
+            var properties = new List<string>();
+
+            var type = typeof(T);
+            IList<PropertyInfo> props = new List<PropertyInfo>(typeof(T).GetProperties());
+
+            foreach (PropertyInfo prop in props)
+            {
+                properties.Add(prop.Name);
+                //object propValue = prop.GetValue(myObject, null);
+
+                //// Do something with propValue
+            }
+
+            return properties;
+
+        }
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using Microsoft.AspNetCore.JsonPatch;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //var props = ExpressionExtensions.GetProperties<Student>();
+            //foreach (var prop in props)
+            //{
+            //    Console.WriteLine(prop);
+            //}
+
+            JsonPatchDocument<Student> studentPatch = new JsonPatchDocument<Student>();
+            studentPatch.Replace(x => x.FirstName, "Hello");
+            //studentPatch.Replace(x => x.Age, 1);
+
+            var firstName = studentPatch.GetValue(x => x.FirstName);
+            var age = (int?)studentPatch.GetValue(x => x.Age);
+            if (age.HasValue)
+            {
+                Console.WriteLine("AA");
+            }
+            //var t = studentPatch.GetType().GetGenericArguments()[0];
+            //var properties = t.GetProperties();
+            Console.ReadKey();
+
+        }
+
+
+    }
+
+    public static class Extension
+    {
+        public static object GetValue<T>(this JsonPatchDocument<T> document, Expression<Func<T, object>> exp = null) where T : class
+        {
+            var me = exp.Body as MemberExpression;
+            if (me == null)
+            {
+                UnaryExpression ubody = (UnaryExpression)exp.Body;
+                me = ubody.Operand as MemberExpression;
+                //throw new ArgumentException("You must pass a lambda of the form: '() => Class.Property' or '() => object.Property'");
+            }
+            var propertyName = me.Member.Name;
+            var propertyInfo = (PropertyInfo)me.Member;
+
+            var propertyType = propertyInfo.PropertyType;
+            var propertyName2 = propertyInfo.Name;
+
+            // check in document
+            var o = document.Operations
+                .FirstOrDefault(x => x.path.ToLower().Equals($@"/{propertyName}".ToLower()));
+
+            return o == null ? null : Convert.ChangeType(o.value, me.Type);
+
+        }
+    }
+
+    public class Student
+    {
+
+        public int Id { get; set; }
+
+        private string PrivateId;
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        protected string PhoneNumber { get; set; }
+
+        public int Age { get; set; }
+
+    }
+}
